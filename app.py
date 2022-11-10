d4a08b754ee8695b5e1c4da9b737024453e417be
Jakub Smékal <jakub.smekal@gmail.com>
2022-11-10 18:33:10 +0100

    removed connect button

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 from wallet_connect import wallet_connect
 
 
-wallet_button = wallet_connect(label="wallet", key="wallet")
-st.write(f"Wallet {wallet_button} connected.")
 login_button = wallet_connect(label="login", key="login", message="Login", auth_nft_contract_address="0x68085453B798adf9C09AD8861e0F0da96B908d81")
 
 if login_button == True:
